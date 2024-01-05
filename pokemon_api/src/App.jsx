308f09ed2d05da3e0596eab59bf2d8b6083e57a9
esohi <esohi@proton.me>
2024-01-05 23:52:22 +0100

    first commit

---
+++ pokemon_api/src/App.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import './App.css'
+
+function App() {
+  const [pokemonList, setPokemonList] = useState([]);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=807');
+  //       if (!response.ok) {
+  //         throw new Error('Network response was not ok');
+  //       }
+
+  //       const data = await response.json();
+  //       setPokemonList(data.results);
+  //     } catch (error) {
+  //       console.error('Fetch error:', error);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []); // Empty dependency array ensures the effect runs only once when the component mounts
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=807')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setPokemonList(data.results);
+      })
+      .catch(error => {
+        console.error('Fetch error:', error);
+      });
+  }, []);
+  
+
+  return (
+    <div className="App">
+      <h1>Pokémon List</h1>
+      <ol>
+        {pokemonList.map((pokemon, index) => (
+          <li key={index}>{pokemon.name}</li>
+        ))}
+      </ol>
+    </div>
+  );
+}
+
+export default App;
