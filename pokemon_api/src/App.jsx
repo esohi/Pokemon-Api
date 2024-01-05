eb2faf76ca527dbc495e746ac2b7ec4e24435b0b
esohi <esohi@proton.me>
2024-01-06 00:12:23 +0100

    second commit

--- pokemon_api/src/App.jsx
+++ pokemon_api/src/App.jsx
@@ -1,8 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import './App.css'
-
-function App() {
-  const [pokemonList, setPokemonList] = useState([]);
+  import React, { useEffect, useState } from 'react';
+  import axios from 'axios';
+  import './App.css';
+  
+  function App() {
+    const [pokemonList, setPokemonList] = useState([]);
+  
+    useEffect(() => {
+      axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=807')
+        .then(response => {
+          if (!response.data) {
+            throw new Error('No data received');
+          }
+          setPokemonList(response.data.results);
+        })
+        .catch(error => {
+          console.error('Axios error:', error);
+        });
+    }, []);
+  
+    return (
+      <div className="App">
+        <h1>Pokémon List</h1>
+        <ol>
+          {pokemonList.map((pokemon, index) => (
+            <li key={index}>{pokemon.name}</li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
+  
+  export default App;
+  
 
   // useEffect(() => {
   //   const fetchData = async () => {
@@ -22,33 +51,33 @@ function App() {
   //   fetchData();
   // }, []); // Empty dependency array ensures the effect runs only once when the component mounts
 
-  useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=807')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        setPokemonList(data.results);
-      })
-      .catch(error => {
-        console.error('Fetch error:', error);
-      });
-  }, []);
+//   useEffect(() => {
+//     fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=807')
+//       .then(response => {
+//         if (!response.ok) {
+//           throw new Error('Network response was not ok');
+//         }
+//         return response.json();
+//       })
+//       .then(data => {
+//         setPokemonList(data.results);
+//       })
+//       .catch(error => {
+//         console.error('Fetch error:', error);
+//       });
+//   }, []);
   
 
-  return (
-    <div className="App">
-      <h1>Pokémon List</h1>
-      <ol>
-        {pokemonList.map((pokemon, index) => (
-          <li key={index}>{pokemon.name}</li>
-        ))}
-      </ol>
-    </div>
-  );
-}
+//   return (
+//     <div className="App">
+//       <h1>Pokémon List</h1>
+//       <ol>
+//         {pokemonList.map((pokemon, index) => (
+//           <li key={index}>{pokemon.name}</li>
+//         ))}
+//       </ol>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
